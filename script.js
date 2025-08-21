d686eda38c5310da87dfb917fcc644cb3bc69258
Ishtiak Mahmood <m64445.0@gmail.com>
2025-08-21 17:27:06 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Fade-in animation for projects
-document.addEventListener('DOMContentLoaded', () => {
-  const projects = document.querySelectorAll('.project');
-  projects.forEach((project, index) => {
-    setTimeout(() => {
-      project.style.opacity = '1';
-    }, 200 * index);
-  });
-});
